e4311cc0910bd504546aea57c7e4768efd68629f
Dryn2007 <adrianadiputra2007@gmail.com>
2025-12-11 08:01:31 +0700

    blok fitur

--- config/features.php
+++ config/features.php
@@ -2,6 +2,14 @@
 
 return[
 
-    'pembayaran_metode' => true,    
+    'pembayaran_metode' => true,
+    'pengeluaran_admin' => true,
+    'grafik_keuangan' => false,
+    'breakdown_pengeluaran' => false,
+    'analisis_hpp' => false,
+    'analisis_margin' => false,
+    'top_produk' => false,
+    'ringkasan_all_time' => false,
+    'laba_bersih' => false,
 
 ];
